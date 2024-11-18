53756e9370a8257843104d2a5b5c5cfd42ed448d
dariopalmesi <dariopalmesi88@gmail.com>
2024-11-18 16:40:19 +0100

    upgrade bug commit

--- src/components/AppMain.jsx
+++ src/components/AppMain.jsx
@@ -1,19 +1,40 @@
+import animes from "../data/anime.js"
+import { useState } from "react"
+
 export default function AppMain() {
+    const [anime, setAnime] = useState(animes)
+    const [newAnime, setNewAnime] = useState('')
+
+    function addAnime(e) {
+        e.preventDefault()
+        console.log(e);
+        setAnime([
+            ...anime,
+            newAnime
+        ])
+        setNewAnime('')
+    }
+
+
+
+
+
+
     return (
         <main>
             <div className="container">
-                <form>
-                    <div className="mb-3">
-                        <label htmlFor="exampleInputEmail1" className="form-label">Email address</label>
-                        <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
+                <form onSubmit={addAnime} className="mt-4">
+                    <div className="input-group mb-3">
+                        <input type="text" className="form-control" placeholder="Recipient's username" aria-label="Recipient's username" aria-describedby="button-addon2" value={newAnime} onChange={e => setNewAnime(e.target.value)} />
+                        <button className="btn btn-outline-secondary" type="submit" id="button-addon2">Send</button>
                     </div>
-                    <div className="mb-3">
-                        <label htmlFor="exampleInputPassword1" className="form-label">Password</label>
-                        <input type="password" className="form-control" id="exampleInputPassword1" />
-                    </div>
-                    <button type="submit" className="btn btn-primary">Submit</button>
+                    <ul className="list-group mt-4">
+                        {animes.map((anime, index) => (< li key={index} className="list-group-item d-flex justify-content-between" >{anime}
+                            <button> <i className="bi bi-trash-fill"></i></button>
+                        </li>))}
+                    </ul>
                 </form>
-            </div>
-        </main>
+            </div >
+        </main >
     )
 }
